b345bada2feace4ecf686f38a3298ef8e499e1ab
Kishori-Tipugade <kishoritipugade18@gmail.com>
2026-04-26 14:43:19 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use("/api", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
